be5e82afd61b084a7431ef4b20dba498d6a60e11
yaelgosman <yael.gos2003@gmail.com>
2025-12-30 17:03:38 +0200

    added router and controller

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const app = express();
-require("dotenv").config();
 const port = process.env.PORT;
 
 const mongoose = require("mongoose");
@@ -10,9 +9,9 @@ const db = mongoose.connection;
 db.on("error", console.error.bind(console, "conenction error:"));
 
 db.once("open", () => {
-    console.log("Connected to the database");
-})
+  console.log("Connected to the database");
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Example app listening at http://localhost:${port}`);
 });
