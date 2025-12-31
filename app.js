97baff810a4aa9cfa3eb3ca2332600914c85e96d
Yaelgos <yael.gos2003@gmail.com>
2025-12-31 12:20:03 +0200

    finished all posts routes and controllers

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const postsRoutes = require("./routes/posts");
+const bodyParser = require("body-parser");
 const port = process.env.PORT;
 
 const mongoose = require("mongoose");
@@ -11,6 +13,10 @@ db.on("error", console.error.bind(console, "conenction error:"));
 db.once("open", () => {
   console.log("Connected to the database");
 });
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/posts", postsRoutes);
 
 app.listen(port, () => {
   console.log(`Example app listening at  http://localhost:${port}`);
--- routes/posts.js
+++ routes/posts.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const router = express.Router();
+const postController = require("../controllers/posts");
 
-const postController = require("../controllers/posts_controller");
+router.post("/", postController.createPost);
 
 router.get("/", postController.getAllPosts);
 
-router.post("/", postController.createPost);
+router.get("/:id", postController.getPostById);
+
+router.get("/", postController.getPostsBySender);
+
+router.put("/:id", postController.updatePost);
 
 module.exports = router;
