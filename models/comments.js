1483b0020e7d9a7e97940d0ea2d3eac69566124c
Yaelgos <yael.gos2003@gmail.com>
2025-12-31 13:03:43 +0200

    added comments model

---
+++ models/comments.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const CommentSchema = new Schema({
+  postId: {
+    type: String,
+    required: true,
+  },
+  content: String,
+  author: {
+    type: String,
+    required: true,
+  },
+});
+
+const Comments = mongoose.model("Comment", CommentSchema);
+module.exports = Comments;
